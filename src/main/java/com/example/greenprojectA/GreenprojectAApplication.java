e2ba1703f4e6d38ad3d0dc51e493cc78e5dab375
chasr0602 <chasr0602@gmail.com>
2025-05-26 10:07:22 +0900

    20250526_1007 프로젝트 초기파일

---
+++ src/main/java/com/example/greenprojectA/GreenprojectAApplication.java
@@ -0,0 +1,13 @@
+package com.example.greenprojectA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenprojectAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenprojectAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/greenprojectA/GreenprojectAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greenprojectA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenprojectAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
